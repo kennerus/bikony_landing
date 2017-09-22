87329eadad8f9b1ea75716b2d7af4e8a1fce69f7
kennerus <kennerus@gmail.com>
2017-09-22 11:31:05 +0300

    адаптация первых двух блоков

--- js/my.js
+++ js/my.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-	$('#nav-icon1').click(function() {
-	$(this).toggleClass('open');
-	$('.header__nav').toggleClass('expand');
+		$('#nav-icon1').click(function() {
+		$(this).toggleClass('open');
+		$('.header__nav').toggleClass('expand');
+		$('.header__solutions').toggleClass('opacity0.3');
 	});
 })
