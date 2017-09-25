941b54a59dbd932d4f991ef1e7dbd4f0a4d0c01c
kennerus <kennerus@gmail.com>
2017-09-25 11:05:00 +0300

    final2

--- js/my.js
+++ js/my.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 		$('#nav-icon1').click(function() {
 		$(this).toggleClass('open');
 		$('.header__nav').toggleClass('expand');
+		$('.header__nav-menu').toggleClass('zindex');
 		$('.header__solutions').toggleClass('opacity0.3');
 	});
 })
